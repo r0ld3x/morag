7bb876b6d4707e44a235f0e9928753d646ae5ab7
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-01-24 19:39:39 +0530

    Update morag.py

--- userbot/plugins/morag.py
+++ userbot/plugins/morag.py
@@ -18,7 +18,6 @@ kraken = bot.uid
 
 PM_IMG = "https://telegra.ph/file/2ebd844b4c5ef8c458b8c.jpg"
 pm_caption = f"🔅 🔅 🔅HEY I AM [MORAG USERBOT](https://t.me/moragmods)🔅 🔅 🔅 THE PRIVATE ASSISTANT OF MY MASTER [{DEFAULTUSER}](tg://user?id={kraken}) \n\n"
-=======
 PM_IMG = "https://telegra.ph/file/2f0a575332c180d75e7b5.png"
 pm_caption = f"🔅 🔅 🔅HEY I AM [ULTRON](https://t.me/rifelmods) PRIME🔅 🔅 🔅 THE PRIVATE ASSISTANT OF MY MASTER [{DEFAULTUSER}](tg://user?id={kraken}) \n\n"
 
@@ -27,12 +26,9 @@ pm_caption += "MY TELETHON VERSION: 1.15.0 \n"
 pm_caption += "MY PYTHON VERSION: 3.7.4 \n"
 
 pm_caption += f"DEVLOPED BY:  [ROLDEX](https://t.me/r0ld3x)\n"
-
-<<<<<<< HEAD:userbot/plugins/morag.py
 pm_caption += "SUPPORT CHANNEL : [HERE](https://t.me/moragmods)\n"
 
 pm_caption += "SUPPORT GROUP : [HERE](https://t.me/moragchats)\n"
-=======
 pm_caption += "SUPPORT CHANNEL : [HERE](https://t.me/rifelmods)\n"
 
 pm_caption += "SUPPORT GROUP : [HERE](https://t.me/rifelmodschats)\n"
